80e4d0037186a2f6cd579a1728651a46f1a54d14
Reiji-Okawa <113846988+Reiji-Okawa@users.noreply.github.com>
2023-03-20 11:32:55 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(function() {
-
+  var count = 0;
+  document.getElementById("hoge").value=count.toString(10);
     $('.lab-link-acordion').click(function(){
         var $answer = $(this).find('.lab-link');
         if($answer.hasClass('open')){
@@ -12,6 +13,28 @@ $(function() {
         }
         
       });
-      
+
+    $('#hoge-btn').click(function() {
+      var count = Number(document.getElementById("hoge").value, 10);
+      count += 1;
+      document.getElementById("hoge").value = count.toString(10);
+    })
+
+    $('.up-btn').click(function() {
+      var tag = document.getElementById('tag');
+      var tag_list = tag.getElementsByTagName('li');
+      var tag_c_1 = tag_list[0];
+      tag.appendChild(tag_c_1);
+    })
+    $('.down-btn').click(function() {
+      var tag = document.getElementById('tag');
+      var tag_list = tag.getElementsByTagName('li');
+      var length = tag_list.length
+      for (var i=0;i<length-1;i++){
+        var tag_c_1 = tag_list[i];
+        tag.appendChild(tag_c_1);  
+      }
+    })
+    
     
   });
